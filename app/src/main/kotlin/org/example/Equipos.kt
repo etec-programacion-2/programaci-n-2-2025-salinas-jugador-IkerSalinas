d48fe45b3e9e8b654c5b12e8568fd7eaa25b68cd
Noah <noahlucianograna@gmail.com>
2025-09-05 10:17:18 -0300

    Equipos y temporadas

---
+++ app/src/main/kotlin/org/example/Equipos.kt
@@ -0,0 +1,8 @@
+package org.example
+
+object Equipos {
+    val gigantes = listOf("Real Madrid", "Manchester City", "Bayern Múnich", "PSG", "Barcelona", "Liverpool")
+    val grandes = listOf("Juventus", "Atlético de Madrid", "Chelsea", "Borussia Dortmund", "Arsenal", "Inter")
+    val normales = listOf("Sevilla", "Napoli", "RB Leipzig", "Tottenham", "Flamengo", "Porto")
+    val mediocres = listOf("Cádiz", "Torino", "Mainz", "Crystal Palace", "Getafe", "Niza")
+}
---
+++ app/src/main/kotlin/org/example/Ligas.kt
@@ -0,0 +1,48 @@
+package org.example
+
+object Ligas {
+    val ligas = mapOf(
+        "Liga Argentina" to mapOf(
+            "Gigantes" to listOf("Boca Juniors", "River Plate"),
+            "Grandes" to listOf("Racing Club", "Independiente"),
+            "Normales" to listOf("San Lorenzo", "Godoy Cruz"),
+            "Mediocres" to listOf("Platense", "Arsenal de Sarandí")
+        ),
+        "Bundesliga" to mapOf(
+            "Gigantes" to listOf("Bayern Múnich", "Borussia Dortmund"),
+            "Grandes" to listOf("Wolfsburg", "Leverkusen"),
+            "Normales" to listOf("Eintracht Frankfurt", "RB Leipzig"),
+            "Mediocres" to listOf("Mainz", "Augsburg")
+        ),
+        "Premier League" to mapOf(
+            "Gigantes" to listOf("Manchester City", "Liverpool"),
+            "Grandes" to listOf("Arsenal", "Chelsea"),
+            "Normales" to listOf("Tottenham", "Newcastle"),
+            "Mediocres" to listOf("Crystal Palace", "Everton")
+        ),
+        "Serie A" to mapOf(
+            "Gigantes" to listOf("Juventus", "Inter"),
+            "Grandes" to listOf("Milan", "Napoli"),
+            "Normales" to listOf("Roma", "Lazio"),
+            "Mediocres" to listOf("Torino", "Sassuolo")
+        ),
+        "Ligue 1" to mapOf(
+            "Gigantes" to listOf("PSG", "Marsella"),
+            "Grandes" to listOf("Monaco", "Lyon"),
+            "Normales" to listOf("Rennes", "Nice"),
+            "Mediocres" to listOf("Toulouse", "Metz")
+        ),
+        "La Liga" to mapOf(
+            "Gigantes" to listOf("Real Madrid", "Barcelona"),
+            "Grandes" to listOf("Atlético de Madrid", "Sevilla"),
+            "Normales" to listOf("Villarreal", "Real Sociedad"),
+            "Mediocres" to listOf("Cádiz", "Getafe")
+        ),
+        "Brasileirao" to mapOf(
+            "Gigantes" to listOf("Flamengo", "Palmeiras"),
+            "Grandes" to listOf("Santos", "Corinthians"),
+            "Normales" to listOf("Fluminense", "Internacional"),
+            "Mediocres" to listOf("Cuiabá", "Goias")
+        )
+    )
+}
